2a2e1f04614c84d18d9e2184ea50e8fa4707d13d
megabyte0x <79016290+megabyte0x@users.noreply.github.com>
2025-06-20 01:00:51 +0800

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { main as update_channel_member_main } from './update_channel_member';
+import { main as update_channel_following_main } from './update_channel_following'
 
 async function main() {
+    await update_channel_following_main();
     await update_channel_member_main();
+
 }
 
 main();
